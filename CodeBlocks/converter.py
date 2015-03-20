b3e08dffb8b8bf970caaaec893560cd7a1e1436f
Vale <valetolpegin@gmail.com>
2015-03-20 19:19:57 -0400

    Updated converted and test code
    - Updated test code to actual test
    - Updated converter to generate basic JS code

--- CodeBlocks/converter.py
+++ CodeBlocks/converter.py
@@ -7,31 +7,50 @@ import os, sys, re
 
 
 #initializing variables
-firstfunction = ""
+firstfunction     = ""
+
+#initializing storage variables for final content
+final_variables   = ""
+final_objects     = ""
+final_content     = ""
 
 
 def filter_comments(text):
     text = re.sub("{{(.*?)}}","",text, flags=re.MULTILINE|re.DOTALL)
     text = re.sub("{(.*?)}","",text, flags=re.MULTILINE|re.DOTALL)
-    text = re.sub("'.*","",text)
     text = re.sub("(?<=\d)_(?=\d)","",text)                           # remove underscores in numbers
+    text = parse_comments( text )
     text = "\n".join([ll.rstrip() for ll in text.splitlines() if ll.strip()])
     text = "\n" + text
     
     return text
 
 
-def filter_operators(text):
-    text = re.sub(":=","=",text)
-    text = re.sub("//","%",text)
-    text = re.sub("=>",">=",text)
-    text = re.sub("=<","<=",text)
-    text = re.sub("@","",text)
-    
+def parse_comments( text ):
+    global final_variables
+    global final_content
+    updated_variables = re.findall( r"'.*", text )
+
+    for var in updated_variables:
+        var = re.sub("'", "", var )
+        var = var.split()
+        
+        for var_component in var:
+            if "VAR_NAME:" in var_component:
+                temp_var = re.sub( "VAR_NAME:", "", var_component )
+                
+                final_variables += " " + temp_var
+                final_content += '\tBlockly.spin.setups_[ "LameStation_' + temp_var + '" ] = "int ' + temp_var + '";\n'
+
+    text = re.sub( "'.*", "", text )
+
     return text
 
 
 def function_2_0( text, label ):
+    global final_variables
+    global final_content
+    
     text = text.split('\n',1)
     prototype = text[0]
     if ':' in prototype:
@@ -59,102 +78,38 @@ def function_2_0( text, label ):
         if firstfunction == None:
             firstfunction = title
 
+    #creating interface code
+    interface_spin_title = "Blockly.Language." + title + " = {\n"
+    interface_spin_code = "\tcategory: 'LameStation',\n\thelpUrl: '',\n\tinit: function() {\n"
+    final_content_ui = ""
+
+    inputs = final_variables.split()
+    for input in inputs:
+        temp_code = "\t\tthis.appendValueInput( " + "'" + input + "'" + ' )\n\t\t\t.appendTitle( "get ' + input + '" );\n'
+
+        final_content_ui += temp_code
+
+    interface_spin_code += final_content_ui + "\t\tthis.setPreviousStatement( true, null );\n\t\tthis.setNextStatement( true, null );\n\t}\n}"
+
+    #creating spin code
     block_spin_title = "Blockly.spin." + title + " = function() {\n"
+    final_content_variables = ""
+
+    inputs = final_variables.split()
+    for input in inputs:
+        final_content_variables += "\tvar " + input + " = Blockly.Spin.valueToCode( this, '" + input + "' );\n"
+
+        text[1] = re.sub( input, '" + ' + input + ' + "', text[1] )
+
     block_spin_code = '\tvar code = "' + text[1] + '";\n\treturn code;'
 
-    text = block_spin_title + block_spin_code + "\n}"
+    final_content_block = final_content + "\n" + final_content_variables + "\n" + block_spin_code + "\n};"
+
+    #creating final code for block
+    text = interface_spin_title + interface_spin_code + "\n\n" + block_spin_title + final_content_block
     
     return text
 
-
-def function(text, label):
-    text = text.split('\n',1)
-    prototype = text[0]
-    if ':' in prototype:
-        if '|' in prototype:
-            title = prototype.split('[:|]')[0]
-            alias=  prototype.split('[:|]')[1]
-            temp =  prototype.split('[:|]')[2]
-        else:
-            title = prototype.split(':')[0]
-            alias = prototype.split(':')[1]
-            temp =  ""
-    else:
-        alias = ""
-        if '|' in prototype:
-            title = prototype.split('|')[0]
-            temp =  prototype.split('|')[1]
-        else:
-            temp = ""
-            title = prototype
-    temps = re.findall(r"[\w]+",temp)
-
-
-    title = title.strip()
-    alias = alias.strip()
-    
-    print title
-    print "  ",alias
-    print "  ",temps
-    
-    
-    if label == 'PUB':
-        global firstfunction
-        if firstfunction == None:
-            firstfunction = title
-
-    # Fix aliases
-    if not alias == "":
-        text[1] = re.sub(alias,"result",text[1])
-        text[1] = filter_operators(text[1])
-    
-    # Fix parameters
-    if not re.search("\(.*\)", title):
-        title += '()'
-
-    # Add parentheses to function calls
-    text[1] = re.sub("(\w+\.\w+)(?![.a-zA-Z_\(])","\g<1>()", text[1])
-
-
-    # eat sub-object hashes
-    text[1] = re.sub("(\w+)#(\w+)","\g<1>.\g<2>", text[1])
-    
-    # handle inc/dec operators
-    text[1] = re.sub("(\w+)\+\+","\g<1> += 1", text[1])
-    text[1] = re.sub("(\w+)--","\g<1> -= 1", text[1])
-    
-    text[1] = re.sub("(\w+\[.+?\])\+\+","\g<1> += 1", text[1])
-    text[1] = re.sub("(\w+\[.+?\])--","\g<1> -= 1", text[1])
-    
-    # flow control
-    text[1] = re.sub("(\s*)repeat[ \t]*\n","\g<1>while True:\n", text[1])    # repeat
-    text[1] = re.sub("(\s*)repeat[ \t]+(\w+)[ \t]+from[ \t]+(.+)[ \t]+to[ \t]+(.+)[ \t]*\n","\g<1>for \g<2> in range(\g<3>, \g<4>):\n", text[1])    # repeat from to
-    text[1] = re.sub("(\s*)until[ \t]+(.*)[ \t]*\n","\g<1>    if \g<2>\g<1>        break\n", text[1])   # until
-    
-    
-    text[1] = re.sub("(\s*if.*)[ \t]*","\g<1>:", text[1])     # repeat
-    text[1] = re.sub("(\s*else.*)[ \t]*","\g<1>:", text[1])     # repeat
-    
-    # miscellaneous
-    text[1] = re.sub("(\w+)\?","random.getrandbits(32)",text[1])        # random
-    text[1] = re.sub("\$([0-9A-Fa-f]+)","int(\"0x\g<1>\",0)",text[1])   # hex
-    text[1] = re.sub("string\((.*?)\)","\g<1>",text[1])                 # string
-    text[1] = re.sub("\",[ \t]*10[ \t]*,\"","\\\n",text[1])             # newlines
-    
-    text[1] = re.sub("cnt","0",text[1])
-    
-    
-    indentlevel = len(text[1].split('\n')[0]) - len(text[1].split('\n')[0].lstrip())
-    #    print indentlevel, text[1]
-    
-    # add function header
-    header = "def " + title + ":\n"
-    for t in temps:
-        header += " "*indentlevel + t + " = 0\n"
-
-    text[1] = header + text[1]
-
-    return text[1]
 
 def data(text, label):
     return ""
@@ -185,7 +140,7 @@ def objects(text, label):
 
 spinblocks = {
     'PUB' : function_2_0,
-    'PRI' : function,
+    'PRI' : function_2_0,
     'DAT' : data,
     'VAR' : variables,
     'CON' : constants,
